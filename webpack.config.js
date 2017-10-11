46aeef8ec3df1d54a1d5dcedb1a02dc5a35fbe9e
hex22a <hex22a@gmail.com>
2017-10-11 03:52:50 +0300

    foo

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,50 @@
-const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-require('./src/styles/main.pcss');
 
 const sourcePath = `${__dirname}/src`;
 const outputPath = `${__dirname}/dist`;
-const entry = `${sourcePath}/index.js`;
+const index = `${sourcePath}/index.pug`;
+const styles = `${sourcePath}/styles/main.pcss`;
 
+
+const extractStyles = new ExtractTextPlugin('[name].css');
+const extractHtml = new ExtractTextPlugin('[name].html');
 
 const config = {
-  entry: { main: entry },
+  stats: {
+    assets: false,
+    colors: true,
+    version: false,
+    hash: true,
+    timings: true,
+    chunks: false,
+    chunkModules: false,
+  },
+  entry: { index, styles },
   output: {
     path: outputPath,
     filename: '[name].js',
   },
   module: {
     rules: [
-      { test: /\.pug$/, use: 'pug-loader' },
+      {
+        test: /\.pug$/,
+        use: extractHtml.extract({
+          use: [
+            { loader: 'html-loader' },
+            {
+              loader: 'pug-html-loader',
+              query: {
+                pretty: true,
+                exports: false,
+              },
+            },
+          ],
+        }),
+      },
       {
         test: /\.pcss$/,
         include: [sourcePath],
-        use: ExtractTextPlugin.extract({
+        use: extractStyles.extract({
           fallback: 'style-loader',
           use: [
             {
@@ -44,8 +68,8 @@ const config = {
     ],
   },
   plugins: [
-
-    new ExtractTextPlugin('[name].css'),
+    extractStyles,
+    extractHtml,
   ],
 };
 
