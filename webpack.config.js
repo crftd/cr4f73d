c0348d7074c65e8110f2268818658a210342fe85
Crash <hex22a@gmail.com>
2018-07-29 10:25:57 +0300

    Update webpack dependency

--- webpack.config.js
+++ webpack.config.js
@@ -133,33 +133,9 @@ const config = {
   plugins: [
     extractStyles,
     extractHtml,
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
   ],
 };
-
-if (isProd) {
-  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: false,
-      screw_ie8: true,
-      conditionals: true,
-      unused: true,
-      comparisons: true,
-      sequences: true,
-      dead_code: true,
-      evaluate: true,
-      if_return: true,
-      join_vars: true,
-    },
-    output: {
-      comments: false,
-    },
-  }));
-} else {
-  config.plugins.push(
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
-  );
-}
-
 
 module.exports = config;
